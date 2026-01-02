ab6df976b08fc2c84dab4611ee72e9f8b8f22b80
tajeelxya-gd <muhammadtajeel@gamedistrict.co>
2026-01-02 10:37:39 +0500

    - update jigsaw piece data

--- unity-client/Assets/Client/Runtime/Puzzle/Core/JigSawPieceData.cs
+++ unity-client/Assets/Client/Runtime/Puzzle/Core/JigSawPieceData.cs
@@ -5,19 +5,15 @@ namespace Client.Runtime
 {
     public sealed class JigSawPieceData
     {
-        private int _originalIdx;
-        private Renderer _renderer;
-        private IEnumerable<JigsawBoardCell> _cells;
-
-        public int OriginalIdx => _originalIdx;
-        public Renderer Renderer => _renderer;
-        public IEnumerable<JigsawBoardCell> Cells => _cells;
+        public int OriginalIdx { get; private set; }
+        public Renderer Renderer { get; private set; }
+        public IEnumerable<JigsawBoardCell> Cells { get; private set; }
 
         public JigSawPieceData(int originalIdx, Renderer renderer, IEnumerable<JigsawBoardCell> cells)
         {
-            _originalIdx = originalIdx;
-            _renderer = renderer;
-            _cells = cells;
+            OriginalIdx = originalIdx;
+            Renderer = renderer;
+            Cells = cells;
         }
     }
 }
