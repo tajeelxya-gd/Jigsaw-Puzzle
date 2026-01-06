3804b7cd4af49d92a064752e10acbbd4512d904f
tajeelxya-gd <muhammadtajeel@gamedistrict.co>
2026-01-06 17:42:45 +0500

    - updated loading widget step

--- unity-client/Assets/Client/Runtime/LoadingSteps/LoadingWidgetStep.cs
+++ unity-client/Assets/Client/Runtime/LoadingSteps/LoadingWidgetStep.cs
@@ -1,18 +1,30 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UniTx.Runtime.Bootstrap;
-using UniTx.Runtime.Widgets;
+using UniTx.Runtime.ResourceManagement;
 using UnityEngine;
 
 namespace Client.Runtime
 {
     public sealed class LoadingWidgetStep : LoadingStepBase
     {
+        [SerializeField] private GameObject _loadingWidget;
         [SerializeField] private bool _activateLoading;
 
         public override UniTask InitialiseAsync(CancellationToken cToken = default)
         {
-            return _activateLoading ? UniWidgets.PushAsync<LoadingWidget>(cToken) : UniWidgets.PopWidgetsStackAsync(cToken);
+            if (_activateLoading)
+            {
+                _loadingWidget.SetActive(true);
+                return UniTask.CompletedTask;
+            }
+
+            if (!UniResources.DisposeInstance(_loadingWidget))
+            {
+                Destroy(_loadingWidget);
+            }
+
+            return UniTask.CompletedTask;
         }
     }
 }
