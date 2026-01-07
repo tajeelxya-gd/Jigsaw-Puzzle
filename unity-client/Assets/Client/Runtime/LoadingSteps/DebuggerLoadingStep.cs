3525f63820c4f2de7a4e76bc541a48f29a4bd014
tajeelxya-gd <muhammadtajeel@gamedistrict.co>
2026-01-07 10:29:02 +0500

    - added debugger loading step

---
+++ unity-client/Assets/Client/Runtime/LoadingSteps/DebuggerLoadingStep.cs
@@ -0,0 +1,19 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UniTx.Runtime.Bootstrap;
+using UnityEngine;
+
+namespace Client.Runtime
+{
+    public sealed class DebuggerLoadingStep : LoadingStepBase
+    {
+        [SerializeField] private bool _activeDebugger;
+        [SerializeField] private GameObject _debugger;
+
+        public override UniTask InitialiseAsync(CancellationToken cToken = default)
+        {
+            _debugger.SetActive(_activeDebugger);
+            return UniTask.CompletedTask;
+        }
+    }
+}
