f247af8a46a527d77ad7caec1a5d3eadf339a892
tajeelxya-gd <muhammadtajeel@gamedistrict.co>
2026-01-08 12:37:41 +0500

    - fixed join collision sizes

--- unity-client/Assets/Client/Runtime/Puzzle/Core/JoinController.cs
+++ unity-client/Assets/Client/Runtime/Puzzle/Core/JoinController.cs
@@ -16,16 +16,16 @@ namespace Client.Runtime
         [SerializeField] private float _tabScaleRatio = 0.2f;
 
         [Tooltip("The actual height/thickness of the joiner tabs")]
-        [SerializeField] private float _tabThickness = 0.01f;
+        [SerializeField] private float _tabThickness = 0.02f;
 
         public void Init(BoxCollider main, JigsawBoardCell[] neighbours, JigSawPiece owner)
         {
-            Vector3 mSize = main.size;
-            Vector3 mCenter = main.center;
+            var mSize = main.size;
+            var mCenter = main.center;
+            var surfaceY = mCenter.y + (mSize.y / 2f);
+            var joinerCenterY = surfaceY;
 
-            float surfaceY = mCenter.y + (mSize.y / 2f);
-            float joinerCenterY = surfaceY + (_tabThickness / 2f);
-
+            // TOP & BOTTOM (Horizontal edges)
             float tbWidth = mSize.x * _tabScaleRatio;
             float tbDepth = tbWidth;
             var tbSize = new Vector3(tbWidth, _tabThickness, tbDepth);
@@ -33,16 +33,19 @@ namespace Client.Runtime
             _top.BoxCollider.size = tbSize;
             _bottom.BoxCollider.size = tbSize;
 
+            // Z positions remain at the front and back edges
             _top.BoxCollider.center = new Vector3(mCenter.x, joinerCenterY, mCenter.z + (mSize.z / 2f));
             _bottom.BoxCollider.center = new Vector3(mCenter.x, joinerCenterY, mCenter.z - (mSize.z / 2f));
 
-            var lrHeight = mSize.z * _tabScaleRatio;
-            var lrDepth = lrHeight;
+            // LEFT & RIGHT (Vertical edges)
+            float lrHeight = mSize.z * _tabScaleRatio;
+            float lrDepth = lrHeight;
             var lrSize = new Vector3(lrDepth, _tabThickness, lrHeight);
 
             _left.BoxCollider.size = lrSize;
             _right.BoxCollider.size = lrSize;
 
+            // X positions remain at the left and right edges
             _left.BoxCollider.center = new Vector3(mCenter.x - (mSize.x / 2f), joinerCenterY, mCenter.z);
             _right.BoxCollider.center = new Vector3(mCenter.x + (mSize.x / 2f), joinerCenterY, mCenter.z);
 
