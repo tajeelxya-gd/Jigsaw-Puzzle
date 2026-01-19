1cae7d8633e36f6aa7b72b07dc9c8e73e8d2f761
tajeelxya-gd <muhammadtajeel@gamedistrict.co>
2026-01-19 11:35:28 +0500

    - updated scene configuration

--- unity-client/Assets/Client/Runtime/LoadingSteps/SceneConfigurationStep.cs
+++ unity-client/Assets/Client/Runtime/LoadingSteps/SceneConfigurationStep.cs
@@ -9,13 +9,29 @@ namespace Client.Runtime
     {
         [SerializeField] private float _refHeight;
         [SerializeField] private float _refWidth;
-        [SerializeField] private Camera _camera;
-        [SerializeField] private Transform _board;
+        [SerializeField] private int _refFOV;
+        [SerializeField] private Camera _mainCamera;
+        [SerializeField] private Transform _puzzleBounds;
 
         public override UniTask InitialiseAsync(CancellationToken cToken = default)
         {
-            // set the camera position such that for ref screen height and width, it matches exactly the same. And for other screen ref and height it adjusts accordingly
+            SetCameraPosition();
             return UniTask.CompletedTask;
+        }
+
+        private void SetCameraPosition()
+        {
+            var refAspect = _refWidth / _refHeight;
+            var currentAspect = (float)Screen.width / Screen.height;
+            var horizontalFOV = 2f * Mathf.Atan(Mathf.Tan(_refFOV * Mathf.Deg2Rad * 0.5f) * refAspect) * Mathf.Rad2Deg;
+            var targetVerticalFOV = 2f * Mathf.Atan(Mathf.Tan(horizontalFOV * Mathf.Deg2Rad * 0.5f) / currentAspect) * Mathf.Rad2Deg;
+
+            _mainCamera.fieldOfView = targetVerticalFOV;
+            var cams = GetComponentsInChildren<Camera>();
+            foreach (var cam in cams)
+            {
+                cam.fieldOfView = targetVerticalFOV;
+            }
         }
     }
 }
