0ced8776f4d2e0130ef75e71839044f7e2853a90
TajeelXya <m.tajeelxya@gmail.com>
2026-02-27 04:52:45 +0500

    - updated pop bulk service

--- unity-client/Assets/GDB/_GameData/Modules/WeeklyMissions/Scripts/PopBulkService.cs
+++ unity-client/Assets/GDB/_GameData/Modules/WeeklyMissions/Scripts/PopBulkService.cs
@@ -5,13 +5,15 @@ using UnityEngine.Events;
 
 public interface IBulkPopService
 {
-    public void PlayEffect(int rewardAmount,PopBulkService.BulkPopUpServiceType serviceType, Vector2 spawnPosition, int animatedCoinsAmount = 10,UnityAction onComplete = null);
+    public void PlayEffect(int rewardAmount, PopBulkService.BulkPopUpServiceType serviceType, Vector2 spawnPosition, int animatedCoinsAmount = 10, UnityAction onComplete = null);
 }
-public class PopBulkService : MonoBehaviour,IBulkPopService
+public class PopBulkService : MonoBehaviour, IBulkPopService
 {
+    // EnemyBlocks -> PuzzleMania
+    // Wand -> Eye
     public enum BulkPopUpServiceType
     {
-        Coins,Trophy,Health,EnemyBlocks, Hammer, Magnets, Wand, SlotPopper
+        Coins, Trophy, Health, EnemyBlocks, Magnets, Wand
     }
     [SerializeField] private BulkPopUpEffect _trophiesGeneratorEffect;
     [SerializeField] private BulkPopUpEffect bulkPopUpEffectEffect;
@@ -27,159 +29,119 @@ public class PopBulkService : MonoBehaviour,IBulkPopService
         GlobalService.RegisterBulkPopService(this);
     }
 
-    public void PlayEffect(int rewardAmount, PopBulkService.BulkPopUpServiceType serviceType, Vector2 spawnPosition, int animatedCoinsAmount = 10,UnityAction onComplete = null)
+    public void PlayEffect(int rewardAmount, PopBulkService.BulkPopUpServiceType serviceType, Vector2 spawnPosition, int animatedCoinsAmount = 10, UnityAction onComplete = null)
     {
         switch (serviceType)
         {
             case BulkPopUpServiceType.Coins:
-            {
-                bulkPopUpEffectEffect.transform.position = spawnPosition;
-                Debug.LogError("adding coin");
-                int baseAmount = rewardAmount / animatedCoinsAmount;
-                int remainder = rewardAmount % animatedCoinsAmount;
-                int index = 0;
-
-                UnityAction action = () =>
                 {
-                    int amount = baseAmount;
-                    if (index < remainder)
-                        amount += 1;
-                    index++;
-                    SignalBus.Publish(new OnCoinsUpdateSignal { Amount = amount });
-                };
+                    bulkPopUpEffectEffect.transform.position = spawnPosition;
+                    Debug.LogError("adding coin");
+                    int baseAmount = rewardAmount / animatedCoinsAmount;
+                    int remainder = rewardAmount % animatedCoinsAmount;
+                    int index = 0;
 
-                bulkPopUpEffectEffect.Generate(null, animatedCoinsAmount, action,onComplete);
-                break;
-            }
-            
+                    UnityAction action = () =>
+                    {
+                        int amount = baseAmount;
+                        if (index < remainder)
+                            amount += 1;
+                        index++;
+                        SignalBus.Publish(new OnCoinsUpdateSignal { Amount = amount });
+                    };
+
+                    bulkPopUpEffectEffect.Generate(null, animatedCoinsAmount, action, onComplete);
+                    break;
+                }
+
             case BulkPopUpServiceType.EnemyBlocks:
-            {
-                _enemyBlockGeneratorEffect.transform.position = spawnPosition;
-
-                int baseAmount = rewardAmount / animatedCoinsAmount;
-                int remainder = rewardAmount % animatedCoinsAmount;
-                int index = 0;
-
-                UnityAction action = () =>
                 {
-                    int amount = baseAmount;
-                    if (index < remainder)
-                        amount += 1;
-                    index++;
-                    SignalBus.Publish(new OnEnemyBlockUpdateSignal() { Amount = amount });
-                };
+                    _enemyBlockGeneratorEffect.transform.position = spawnPosition;
 
-                _enemyBlockGeneratorEffect.Generate(null, animatedCoinsAmount, action,onComplete);
-                break;
-            }
+                    int baseAmount = rewardAmount / animatedCoinsAmount;
+                    int remainder = rewardAmount % animatedCoinsAmount;
+                    int index = 0;
+
+                    UnityAction action = () =>
+                    {
+                        int amount = baseAmount;
+                        if (index < remainder)
+                            amount += 1;
+                        index++;
+                        SignalBus.Publish(new OnEnemyBlockUpdateSignal() { Amount = amount });
+                    };
+
+                    _enemyBlockGeneratorEffect.Generate(null, animatedCoinsAmount, action, onComplete);
+                    break;
+                }
 
             case BulkPopUpServiceType.Trophy:
-            {
-                _trophiesGeneratorEffect.transform.position = spawnPosition;
-                _trophiesGeneratorEffect.Generate(null, animatedCoinsAmount);
-                break;
-            }
+                {
+                    _trophiesGeneratorEffect.transform.position = spawnPosition;
+                    _trophiesGeneratorEffect.Generate(null, animatedCoinsAmount);
+                    break;
+                }
 
             case BulkPopUpServiceType.Health:
-            {
-                _hearsGeneratorEffect.transform.position = spawnPosition;
-
-                int baseAmount = rewardAmount / animatedCoinsAmount;
-                int remainder = rewardAmount % animatedCoinsAmount;
-                int index = 0;
-
-                UnityAction action = () =>
                 {
-                    int amount = baseAmount;
-                    if (index < remainder)
-                        amount += 1;
-                    index++;
-                    SignalBus.Publish(new OnHealthUpdateSignal { TimeToAdd = amount });
-                };
+                    _hearsGeneratorEffect.transform.position = spawnPosition;
 
-                _hearsGeneratorEffect.Generate(null, animatedCoinsAmount, action,onComplete);
-                break;
-            }
-            case BulkPopUpServiceType.Hammer:
-            {
-                _hammerGeneratorEffect.transform.position = spawnPosition;
+                    int baseAmount = rewardAmount / animatedCoinsAmount;
+                    int remainder = rewardAmount % animatedCoinsAmount;
+                    int index = 0;
 
-                int baseAmount = rewardAmount / animatedCoinsAmount;
-                int remainder = rewardAmount % animatedCoinsAmount;
-                int index = 0;
+                    UnityAction action = () =>
+                    {
+                        int amount = baseAmount;
+                        if (index < remainder)
+                            amount += 1;
+                        index++;
+                        SignalBus.Publish(new OnHealthUpdateSignal { TimeToAdd = amount });
+                    };
 
-                UnityAction action = () =>
-                {
-                    int amount = baseAmount;
-                    if (index < remainder)
-                        amount += 1;
-                    index++;
-                    SignalBus.Publish(new OnHammerUpdateSignal { Amount = amount });
-                };
-
-                _hammerGeneratorEffect.Generate(null, animatedCoinsAmount, action,onComplete);
-                break;
-            }
+                    _hearsGeneratorEffect.Generate(null, animatedCoinsAmount, action, onComplete);
+                    break;
+                }
             case BulkPopUpServiceType.Wand:
-            {
-                _wandGeneratorEffect.transform.position = spawnPosition;
-
-                int baseAmount = rewardAmount / animatedCoinsAmount;
-                int remainder = rewardAmount % animatedCoinsAmount;
-                int index = 0;
-
-                UnityAction action = () =>
                 {
-                    int amount = baseAmount;
-                    if (index < remainder)
-                        amount += 1;
-                    index++;
-                    SignalBus.Publish(new OnWandUpdateSignal { Amount = amount });
-                };
+                    _wandGeneratorEffect.transform.position = spawnPosition;
 
-                _wandGeneratorEffect.Generate(null, animatedCoinsAmount, action,onComplete);
-                break;
-            }
-            case BulkPopUpServiceType.SlotPopper:
-            {
-                _slotPopperGeneratorEffect.transform.position = spawnPosition;
+                    int baseAmount = rewardAmount / animatedCoinsAmount;
+                    int remainder = rewardAmount % animatedCoinsAmount;
+                    int index = 0;
 
-                int baseAmount = rewardAmount / animatedCoinsAmount;
-                int remainder = rewardAmount % animatedCoinsAmount;
-                int index = 0;
+                    UnityAction action = () =>
+                    {
+                        int amount = baseAmount;
+                        if (index < remainder)
+                            amount += 1;
+                        index++;
+                        SignalBus.Publish(new OnWandUpdateSignal { Amount = amount });
+                    };
 
-                UnityAction action = () =>
-                {
-                    int amount = baseAmount;
-                    if (index < remainder)
-                        amount += 1;
-                    index++;
-                    SignalBus.Publish(new OnSlotPopperUpdateSignal { Amount = amount });
-                };
-
-                _slotPopperGeneratorEffect.Generate(null, animatedCoinsAmount, action,onComplete);
-                break;
-            }
+                    _wandGeneratorEffect.Generate(null, animatedCoinsAmount, action, onComplete);
+                    break;
+                }
             case BulkPopUpServiceType.Magnets:
-            {
-                _magnetGeneratorEffect.transform.position = spawnPosition;
-
-                int baseAmount = rewardAmount / animatedCoinsAmount;
-                int remainder = rewardAmount % animatedCoinsAmount;
-                int index = 0;
-
-                UnityAction action = () =>
                 {
-                    int amount = baseAmount;
-                    if (index < remainder)
-                        amount += 1;
-                    index++;
-                    SignalBus.Publish(new OnMagnetUpdateSignal { Amount = amount });
-                };
+                    _magnetGeneratorEffect.transform.position = spawnPosition;
 
-                _magnetGeneratorEffect.Generate(null, animatedCoinsAmount, action,onComplete);
-                break;
-            }
+                    int baseAmount = rewardAmount / animatedCoinsAmount;
+                    int remainder = rewardAmount % animatedCoinsAmount;
+                    int index = 0;
+
+                    UnityAction action = () =>
+                    {
+                        int amount = baseAmount;
+                        if (index < remainder)
+                            amount += 1;
+                        index++;
+                        SignalBus.Publish(new OnMagnetUpdateSignal { Amount = amount });
+                    };
+
+                    _magnetGeneratorEffect.Generate(null, animatedCoinsAmount, action, onComplete);
+                    break;
+                }
 
             default:
                 throw new ArgumentOutOfRangeException(nameof(serviceType), serviceType, null);
@@ -189,8 +151,8 @@ public class PopBulkService : MonoBehaviour,IBulkPopService
     [Button]
     public void TestAddMagnets()
     {
-        PlayEffect(10,PopBulkService.BulkPopUpServiceType.Magnets,transform.position, 4);
-        
+        PlayEffect(10, PopBulkService.BulkPopUpServiceType.Magnets, transform.position, 4);
+
     }
 
 
