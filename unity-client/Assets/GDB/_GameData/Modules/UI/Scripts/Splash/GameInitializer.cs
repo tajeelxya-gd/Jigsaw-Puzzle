5d0e9081a08e6390e3917080e22b66876fd1ac33
TajeelXya <muhammadtajeel@gamedistrict.co>
2026-02-27 09:44:43 +0500

    - updated initializer

--- unity-client/Assets/GDB/_GameData/Modules/UI/Scripts/Splash/GameInitializer.cs
+++ unity-client/Assets/GDB/_GameData/Modules/UI/Scripts/Splash/GameInitializer.cs
@@ -23,6 +23,7 @@ public class GameInitializer : MonoBehaviour
 
     private void OnEnable()
     {
+        Load();
         // _privacyPanel.SetActive(PlayerPrefs.GetInt("PrivacyPolicy", 0) == 0);
         // PrivacyPolicyPanel.OnPolicyAcceptedEvent += Load;
     }
