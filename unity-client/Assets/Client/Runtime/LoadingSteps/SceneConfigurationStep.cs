e39ae1ec1f4720b32ac85dec494b914b399ff04d
TajeelXya <muhammadtajeel@gamedistrict.co>
2026-03-02 11:50:28 +0500

    - added scene configuration step

--- unity-client/Assets/Client/Runtime/LoadingSteps/SceneConfigurationStep.cs
+++ unity-client/Assets/Client/Runtime/LoadingSteps/SceneConfigurationStep.cs
@@ -7,32 +7,36 @@ namespace Client.Runtime
 {
     public sealed class SceneConfigurationStep : LoadingStepBase
     {
-        [SerializeField] private Transform _camera;
+        [SerializeField] private float _fieldOfView;
+        [SerializeField] private Camera[] _cameras;
 
         public override async UniTask InitialiseAsync(CancellationToken cToken = default)
         {
             // Wait a frame to ensure correct screen dimensions
             await UniTask.Yield(PlayerLoopTiming.Update);
 
-            SetCameraHeight();
+            AdjustCamera();
         }
 
-        private void SetCameraHeight()
+        private void AdjustCamera()
         {
-            float currentWidth = Screen.currentResolution.width;
-            float currentHeight = Screen.currentResolution.height;
-            float currentAspect = currentWidth / currentHeight;
+            float baseAspect = 9f / 16f;
+            float currentAspect = (float)Screen.width / Screen.height;
 
-            // Rest of your logic remains the same
-            float refAspect1 = 1920f / 1080f;
-            float refAspect2 = 1344f / 2992f;
-
-            float targetY = Mathf.Abs(currentAspect - refAspect1) < Mathf.Abs(currentAspect - refAspect2)
-                ? 1.05f : 1.35f;
-
-            Vector3 pos = _camera.localPosition;
-            pos.y = targetY;
-            _camera.localPosition = pos;
+            if (currentAspect > baseAspect)
+            {
+                foreach (var camera in _cameras)
+                {
+                    camera.fieldOfView = _fieldOfView;
+                }
+            }
+            else
+            {
+                foreach (var camera in _cameras)
+                {
+                    camera.fieldOfView = _fieldOfView * (baseAspect / currentAspect);
+                }
+            }
         }
     }
 }
