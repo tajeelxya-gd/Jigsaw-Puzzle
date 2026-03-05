f0c54b3f826b259f7c9cdfef2764a7f3660cd1a9
TajeelXya <m.tajeelxya@gmail.com>
2026-03-06 00:49:02 +0500

    - fixed puzzle mania progress bar

--- unity-client/Assets/GDB/_GameData/Modules/Events/PuzzleMania/Runtime/Scripts/PuzzleManiaBar.cs
+++ unity-client/Assets/GDB/_GameData/Modules/Events/PuzzleMania/Runtime/Scripts/PuzzleManiaBar.cs
@@ -15,21 +15,6 @@ public class PuzzleManiaBar : MonoBehaviour
     [SerializeField] public float _fillDuration = 0.5f;
     [SerializeField] private UIParticle _particle;
     [SerializeField] private PanelScaling _scaling;
-    private bool[] _hasReachedMax;
-
-    private List<float> _maxBarWidth = new List<float>();
-
-    void Awake()
-    {
- 
-        _hasReachedMax = new bool[_fillBar.Length];
-
-        for (int i = 0; i < _fillBar.Length; i++)
-        {
-            _maxBarWidth.Add(_fillBar[i].rectTransform.sizeDelta.x);
-            _hasReachedMax[i] = false;
-        }
-    }
 
     [Button]
     public void UpdateFillBar(float progress)
@@ -38,34 +23,11 @@ public class PuzzleManiaBar : MonoBehaviour
 
         for (int i = 0; i < _fillBar.Length; i++)
         {
-            int index = i; 
-
-            RectTransform rt = _fillBar[index].rectTransform;
-            Vector2 temp = rt.sizeDelta;
-
-            float maxWidth = _maxBarWidth[index];
-            float targetWidth = maxWidth * progress;
-
-            rt.DOKill();
-
-            if (_hasReachedMax[index])
-            {
-                rt.sizeDelta = new Vector2(0, temp.y);
-                _hasReachedMax[index] = false;
-            }
-
-            rt.DOSizeDelta(new Vector2(targetWidth, temp.y), _fillDuration)
-                .SetEase(Ease.OutCubic)
-                .OnComplete(() =>
-                {
-                    if (targetWidth >= maxWidth - 0.5f)
-                    {
-                        _hasReachedMax[index] = true;
-                    }
-                });
+            var img = _fillBar[i];
+            img.DOKill();
+            img.DOFillAmount(progress, _fillDuration).SetEase(Ease.OutCubic);
         }
     }
-
 
     public void UpdateProgressText(int current, int total)
     {
@@ -83,7 +45,7 @@ public class PuzzleManiaBar : MonoBehaviour
     {
         _scaling.ScaleOut();
         for (int i = 0; i < _rewardToBeGiven.Length; i++)
-        {        
+        {
             //_rewardToBeGiven[i].SetNativeSize();
             _rewardToBeGiven[i].gameObject.SetActive(false);
             yield return new WaitForSeconds(.25f);
